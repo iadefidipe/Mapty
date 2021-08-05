9e5e88a03839b6ab515ee627d568c419407c28ad
Israel Adefidipe <israeladefidipe@gmail.com>
2021-08-05 01:57:51 +0100

    Mapty: Displaying a map using a 3rd party library leaflet

--- script.js
+++ script.js
@@ -20,12 +20,31 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 */
 
-navigator.geolocation.getCurrentPosition( function(position){ //!the success callback function is usually called with a position  parameter
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    //!the success callback function is usually called with a position  parameter
     console.log(position);
-    const {latitude,longitude} = position.coords;
-    console.log(latitude, longitude)
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+    const { latitude, longitude } = position.coords;
+    console.log(latitude, longitude);
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-}, function(){
-    alert ('Could not get your postion')
-})
+    const coords = [latitude, longitude]
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+  },
+  function () {
+    alert('Could not get your postion');
+  }
+);
+
+//  TODO: Displaying a map using a 3rd party library leaflet
+// ? CDN => COntent delivery network
